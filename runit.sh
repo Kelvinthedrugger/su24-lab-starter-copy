91257095c75211f9f05e9c3ba34dd52afe987e9c
Kelvinthedrugger <890225kelvin@gmail.com>
2024-07-25 18:12:11 +0800

    we run with this, mnt dir co-edit not done

--- runit.sh
+++ runit.sh
@@ -1,3 +1,6 @@
 
 # use 'docker ps' command to check if there're containers hanging
-docker run --privileged -v "$PWD"/projects:/workspace/projects --name cs61c_24_kel -it --rm cs61c_24_kel bash
+#docker run --privileged -v "$PWD"/projects:/workspace/projects --name cs61c_24_kel -it --rm cs61c_24_kel bash
+
+# i used this last time
+docker run --privileged -v "$PWD"/proj --name cs61c_24_kel -it --rm cs61c_24_kel bash
