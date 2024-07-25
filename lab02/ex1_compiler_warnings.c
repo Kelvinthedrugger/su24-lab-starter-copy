9fb956e6a589a92545a931ca8e8470a1fba8327e
Kelvinthedrugger <890225kelvin@gmail.com>
2024-07-25 19:36:58 +0800

    lab02 ex1 done, now procrastinating on installing gdb

--- lab02/ex1_compiler_warnings.c
+++ lab02/ex1_compiler_warnings.c
@@ -10,9 +10,9 @@ struct Course {
 struct Course *make_course(int id, char *name) {
     struct Course *new_course = malloc(sizeof(struct Course));
     new_course->id = id;
-    new_course->name = *name;
+    new_course->name = name; // error no.1
 
-    return &new_course;
+    return new_course; // the other 2 errors
 }
 int main() {
     struct Course *cs161 = make_course(161, "Computer Security");
